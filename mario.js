de53e1ce63f0314d5967daddd1192a76c0b54bd5
Arbaj Office PC <arbajmessiali@outlook.com>
2023-12-20 15:21:04 +0530

    add pull down/gravity element

--- mario.js
+++ mario.js
@@ -1,20 +1,40 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+canvas.width = innerWidth
+canvas.height = innerHeight
+
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
         }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
         this.width = 100
         this.height = 100
     }
 
     draw() {
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 1
     }
 }
 
 const player = new Player()
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0,0,canvas.width,canvas.height)
+    player.update()
+}
+
+animate()
