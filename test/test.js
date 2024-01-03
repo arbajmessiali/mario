615657bae67cf57d22027b203ec0ac591fac5297
Arbaj Ali <arbajmessiali@outlook.com>
2024-01-03 20:15:39 +0530

    test startScreen to trigger game (success)

--- test/test.js
+++ test/test.js
@@ -1,24 +1,31 @@
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
+function loading(){
+    show = function(){
         document.querySelector(
-            "body").style.visibility = "hidden";
+            "#loader").style.display = "block";
         document.querySelector(
-            "#loader").style.visibility = "visible";
-    } else {
+            "canvas").style.visibility = "hidden";  
+        setTimeout(hide, 2000); // 2 seconds
+      },
+
+      hide = function(){
         document.querySelector(
             "#loader").style.display = "none";
         document.querySelector(
-            "body").style.visibility = "visible";
-    }
+            "canvas").style.visibility = "visible";
+      };
+    show();
 };
+
+loading();
 
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 200;
-const CANVAS_HEIGHT = canvas.height = 200;
+const CANVAS_WIDTH = canvas.width = 400;
+const CANVAS_HEIGHT = canvas.height = 400;
 
 const imag = new Image();
 imag.src = '../img/hero-sheet.png';
+load_src = '../img/buttons.png';
 
 const spriteWidth = 32
 const spriteHeight = 32
@@ -29,6 +36,13 @@ let frameR = 1
 let frameX = 0
 let gameFrame = 0
 let frameHold = 7
+
+function createImg(imgSrc){
+    const image = new Image()
+    image.src = imgSrc
+    return image
+}
+
 
 class StartScreen {
     constructor({x, y, w, h}) {
@@ -42,8 +56,8 @@ class StartScreen {
     }
 
     draw() {
-        c.drawImage(createImg(load_src), 96, 32, 48, 16, this.position.x, this.position.y, this.width, this.height)
-        c.drawImage(createImg(load_src), 288, 32, 48, 16, this.position.x, this.position.y + 30, this.width, this.height)
+        ctx.drawImage(createImg(load_src), 96, 32, 48, 16, this.position.x, this.position.y, this.width, this.height)
+        ctx.drawImage(createImg(load_src), 288, 32, 48, 16, this.position.x, this.position.y + 30, this.width, this.height)
        /* c.fillStyle = 'blue'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
         */
@@ -53,10 +67,12 @@ class StartScreen {
 let start
 
 function startScreen(){
-    start = new StartScreen({x:200, y:200, w:100, h:32})
+    start = new StartScreen({x:100, y:100, w:100, h:32})
     start.draw()
-    if(keys.up.pressed)
-        c.fillRect(190,200,10,32)
+    if(keys.select.pressed)
+        ctx.fillRect(85,100,10,32)
+    else
+        ctx.fillRect(85,130,10,32)
 }
 
 const keys = {
@@ -64,6 +80,12 @@ const keys = {
         pressed: false
     },
     left : {
+        pressed: false
+    },
+    select : {
+        pressed: true
+    },
+    space : {
         pressed: false
     }
 }
@@ -128,17 +150,35 @@ addEventListener('keydown', ({keyCode}) => {
             console.log('left')
             break;
         case 83:
+            if(keys.select.pressed)
+                keys.select.pressed = false
+            else
+                keys.select.pressed = true
             break;
         case 68:
             keys.right.pressed = true
             anim.right.pressed = true
             console.log('right')
             break;
+    }
+})
+
+addEventListener('keypress', ({keyCode}) => {
+    switch(keyCode){
         case 87:
             player.velocity.y -= 20
+            if(keys.select.pressed)
+                keys.select.pressed = false
+            else
+                keys.select.pressed = true
+            break;
+        case 32:
+                keys.space.pressed = true
             break;
     }
 })
+
+let startGame = false
 
 addEventListener('keyup', ({keyCode}) => {
     switch(keyCode){
@@ -152,18 +192,21 @@ addEventListener('keyup', ({keyCode}) => {
             keys.right.pressed = false
             anim.right.pressed = false
             break;
-        case 87:
-            player.velocity.y = 0
-            break;
     }
 })
 
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    player.draw()
     gameFrame++
     requestAnimationFrame(animate);
     startScreen()
+    if(keys.select.pressed && keys.space.pressed){
+        startGame = true
+        keys.space.pressed = false
+    }
+
+    if(startGame)
+        player.draw()
 
     if(keys.right.pressed && player.position.x < 400){
         player.velocity.x = 5
